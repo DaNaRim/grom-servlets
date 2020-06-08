3a82b16e7fef6b00c589454ab43ec9cbf061294f
DaNaRim <rangar.danarim@gmail.com>
2020-06-09 01:01:35 +0300

    debug7

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -1,9 +1,11 @@
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(value = "/test")
 public class MyServlet extends HttpServlet {
 
     @Override
